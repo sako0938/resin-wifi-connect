4afc37ebba434ef56857b574e5fd54a67ab3bcb6
John (Jack) Brown <jack@brownjohnf.com>
2017-09-19 07:11:54 -0700

    debug

--- ci/build.sh
+++ ci/build.sh
@@ -9,6 +9,7 @@ fi
 
 docker build -t $TARGET ci/docker/$TARGET
 
+docker run -it --rm -v ${PWD}:/work $TARGET ls -lah
 docker run -it --rm -v ${PWD}:/work $TARGET cargo build --release --target=$TARGET
 
 docker run -it --rm -v ${PWD}:/work $TARGET stripbin target/$TARGET/release/$BINARY
